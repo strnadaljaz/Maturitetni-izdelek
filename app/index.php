846a6141628a718000726633ebb261645a388479
astrnad1 <118618076+astrnad1@users.noreply.github.com>
2024-10-21 18:18:44 +0200

    novi design appa

--- app/index.php
+++ app/index.php
@@ -74,7 +74,7 @@ if ($username) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task Manager</title>
+    <title>ToDo app</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
